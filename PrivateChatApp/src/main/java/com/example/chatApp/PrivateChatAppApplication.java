850d908f82d895a0fd2f1f62ab6d9c3dd2c184ec
PhillipVolkov <phillipvolkov@gmail.com>
2021-05-20 12:22:24 -0700

    Initial Spring creation

---
+++ PrivateChatApp/src/main/java/com/example/chatApp/PrivateChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateChatAppApplication.class, args);
+	}
+
+}
